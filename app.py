68b913cdc721b4b2a6ba863bd14b41d06e5c729d
Angelica Farias Oliveira <angelica.oliveira@inmetrics.com.br>
2024-07-15 14:39:01 -0300

    Projeto em Python

---
+++ app.py
@@ -0,0 +1,79 @@
+# Importando biblioteca os
+import os
+
+restaurantes = ['Pizza', 'Sushi']
+
+def exibir_nome_do_programa():
+
+    print("""
+      𝓓𝓮𝓵𝓲𝓬𝓲𝓪 𝓥𝓮𝓰𝓪𝓷𝓪
+      
+      """)
+
+def exibir_opcoes():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurantes')
+    print('3. Ativar restaurante') # restaurante precisa ser cadastrado para aparecer no app.
+    print('4. Sair\n')
+
+def finalizar_app():
+    exibir_subtitulo('Finalziar app')
+
+def voltar_ao_menu_principal():
+    input('\nDigite uma tecla para voltar ao menu principal')
+    main()
+
+def opcao_invalida():
+    print('Opção inválida!\n')
+    voltar_ao_menu_principal()
+
+def exibir_subtitulo(texto):
+    os.system('cls')
+    print(texto)
+    print()
+
+def cadastrar_novo_restaurante():
+    exibir_subtitulo('Cadastro de novos restaurantes\n')
+    nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
+    restaurantes.append(nome_do_restaurante)
+    print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
+    
+    voltar_ao_menu_principal()
+    
+def listar_restaurantes():
+    exibir_subtitulo('Listando restaurantes')
+
+    for restaurante in restaurantes:
+        print(f'.{restaurante}')
+
+    voltar_ao_menu_principal()
+
+def escolher_opcao():
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
+        # opcao_escolhida = input('Escolha uma opção: ')
+        # print(f'Você escolheu a oção {opcao_escolhida}')
+
+        if opcao_escolhida == 1:
+            cadastrar_novo_restaurante()
+        elif opcao_escolhida == 2:
+            listar_restaurantes()
+        elif opcao_escolhida == 3:
+            print('Ativar restaurantes')
+        elif opcao_escolhida == 4:
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()
+
+# criada a função main e definição de todos os passos para o programa funcionar
+def main():
+    os.system('cls')
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+
+if __name__== '__main__':
+    main()
